0547c9c3405252ee4cbb19c528a1398ee27fecc3
mrinmoygithub <mrinmoy.r@xelpmoc.in>
2024-07-08 21:29:02 +0530

    database connection

--- projectDescription.md
+++ projectDescription.md
@@ -11,3 +11,26 @@
 mkdir controllers, db, middlewares, models, routes, utils
 - npm i -D nodemon 
 - npm i -D prettier
+
+# DB connection 
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,50 @@
+// require('dotenv').config({path: './env'})
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+// function connectDB(){}
+
+// connectDB()
+// ifies
+
+(async() => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("ERROR:", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+*/
